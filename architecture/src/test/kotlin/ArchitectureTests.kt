a2376896d1bb926c46e3a3995e7d2b5d0fbd02b6
Jean-Michel Cazaux <jeanmichel.cazaux@gmail.com>
2024-10-06 14:36:45 +0200

    Improving repro-case

--- architecture/src/test/kotlin/ArchitectureTests.kt
+++ architecture/src/test/kotlin/ArchitectureTests.kt
@@ -6,16 +6,29 @@ import io.kotest.core.spec.style.FreeSpec
 class ArchitectureTests: FreeSpec({
 
     val scope = Konsist.scopeFromProject() - Konsist.scopeFromTest()
+    val appLayer = Layer("app", "com.ironbird.app..")
+    val libLayer = Layer("lib", "com.ironbird.lib..")
 
     println("*".repeat(80))
     println(scope)
     println("*".repeat(80))
 
+    // This fails (as expected)
+    // com.ironbird.app.Main.kt imports com.ironbird.lib.ia.NumberGuesser
+    "app should not depend on lib" {
+        scope.assertArchitecture {
+            appLayer.doesNotDependOn(libLayer)
+        }
+    }
+
     // This should fail.
     // com.ironbird.app.Main.kt imports com.ironbird.lib.ia.NumberGuesser
     "app should depend on nothing" {
         scope.assertArchitecture {
-            val appLayer = Layer("app", "com.ironbird.app..")
+            appLayer.dependsOnNothing()
+        }
+        // Or this...
+        scope.files.assertArchitecture {
             appLayer.dependsOnNothing()
         }
     }
@@ -24,9 +37,12 @@ class ArchitectureTests: FreeSpec({
     // com.ironbird.lib.ia.NumberGuesser does NOT use anything in com.ironbird.app.* (it is the other way around)
     "lib should depend on app" {
         scope.assertArchitecture {
-            val appLayer = Layer("app", "com.ironbird.app..")
-            val libLayer = Layer("lib", "com.ironbird.lib..")
             libLayer.dependsOn(appLayer)
         }
+        //Or this
+        scope.files.assertArchitecture {
+            libLayer.dependsOn(appLayer)
+        }
+
     }
 })
